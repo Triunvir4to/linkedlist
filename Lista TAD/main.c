f02fbaa6a86d561cc5012cd50441ddacaf72d38d
Daniel Arrudas de Macêdo <100447748+DanielArrudas@users.noreply.github.com>
2024-03-26 16:48:15 -0300

    Update main.c

--- Lista TAD/main.c
+++ Lista TAD/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "lista.h"
 
 int main(){
 
+    Lista *L1;
+    int v, num;
+    char excluir, buscar;
+
+    printf("Lista\nQuantos dados: ");
+    scanf(" %d", &num);
+
+    for(int i = 1; i <= num; i++){
+        printf("Digite valores: ");
+        scanf(" %d", &v);
+        L1 = ListaInserirNoFim( L1, v);
+    }
+
+    printf("Deseja excluir um elemento da lista? (S ou N)");
+    scanf(" %c", &excluir);
+    excluir = toupper(excluir);
+    if(excluir == 'S'){
+        printf("Qual elemento? ");
+        scanf(" %c", &excluir);
+        ListaExcluir(L1,excluir);
+    }
+
+    printf("Valores da lista:\n");
+    ListaImprimir(L1);
+
+    ListaLiberar(L1);
 }
